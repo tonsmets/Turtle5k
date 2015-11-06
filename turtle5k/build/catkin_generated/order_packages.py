127fb0daa4183fe0006c1894008407875bd04f94
JtotheZ <joury.zimmermann@gmail.com>
2015-11-06 10:42:26 +0100

    Removed old ROS folder

--- turtle5k/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/student/turtle5k/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/student/turtle5k/devel;/opt/ros/indigo".split(';') if "/home/student/turtle5k/devel;/opt/ros/indigo" != "" else []
