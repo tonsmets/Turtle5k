3f94dc4575388183114008103abdb8c644372cbd
Your Name <you@example.com>
2015-11-19 11:59:53 +0100

    Shooting test stub

---
+++ turtle5k/src/stubs/shootingLeverStub.cpp
@@ -0,0 +1,47 @@
+#include <string>
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include "../hardware/headers/ShootingLeverTestStub.h"
+
+ros::Publisher pShootingInfoPub;
+ros::Subscriber pShootingCmdSub;
+
+ShootingLeverTestStub shootingLeverTestStub;
+
+void frameCallback(const std_msgs::String::ConstPtr& aMessage)
+{
+	ROS_INFO("[SHOOTING] Got %s, For: Shooting", aMessage->data.c_str());
+
+	int newAngle = atoi(aMessage->data.c_str());
+	shootingLeverTestStub.setAngle(newAngle);
+	ROS_INFO("setShootingAngle: [%i]", newAngle);
+}
+
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "t5k-tactics");
+
+	ros::NodeHandle pHandle;
+	ros::Rate pRate(24);
+
+	pShootingInfoPub = pHandle.advertise<std_msgs::String>("/t5k/shootinginfo", 1000);
+	pShootingCmdSub = pHandle.subscribe("/t5k/shootingcommands", 1000, frameCallback);
+
+	while(ros::ok()) {
+
+		double angle = shootingLeverTestStub.getAngle();
+
+		std_msgs::String pMessage;
+		pMessage.data = std::to_string(angle);
+
+		ROS_INFO_STREAM("Shooting angle: " << pMessage.data << std::endl);
+
+		pShootingInfoPub.publish(pMessage);
+		ros::spinOnce();
+		pRate.sleep();
+	}
+
+	return 0;
+}
+
