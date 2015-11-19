a9b810fdaa4261170f52060b6fc80aa400c6657b
Laurence Saes <info@lsaes.nl>
2015-11-19 12:39:47 +0100

    Better test data. No more random

--- turtle5k/src/hardware/CompassTestStub.cpp
+++ turtle5k/src/hardware/CompassTestStub.cpp
@@ -1,7 +1,14 @@
 #include "headers/CompassTestStub.h"
 #include <stdlib.h>
 
+CompassTestStub::CompassTestStub() {
+	this->lastDegrees = 0;
+}
+
 double CompassTestStub::getDegrees()
 {
-	return static_cast<double>(rand() % 3600) / 10.0;
+	this->lastDegrees += static_cast<double>(rand() % 3600) / 1000.0;
+	if(this->lastDegrees > 360)
+		this->lastDegrees = 0;
+	return this->lastDegrees ;
 }
