c3163989d7dbc007ccd7cf86244d39b66ff5595d
nfrancke <nfrancke@student.avans.nl>
2016-01-06 14:45:21 +0100

    changed startup files
    	modified:   linuxScript/ControlMotorsWithJoystickTwistStartupFile.sh
    	modified:   linuxScript/README.md
    	new file:   linuxScript/ssh$HOSTNAMEstartup.sh

--- linuxScript/ControlMotorsWithJoystickTwistStartupFile.sh
+++ linuxScript/ControlMotorsWithJoystickTwistStartupFile.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 # /etc/init.d/ControlMotorsWithJoystickTwistStartupFile
 
-#Path to ROS and Workspace
 . /opt/ros/indigo/setup.sh	
 . ~/catkin_ws/devel/setup.sh
 
-sleep 20 #wait 15 seconds for opening the ports 5 7 en 8
+sleep 20 #wait 20 seconds for opening the ports 5 7 en 8
+
+valhost=$(hostname)
+echo $valhost
+export ROS_MASTER_URI=http://$valhost:11311
 
 # Path to ROS workspace and starting the ros launch file
 export ROS_PACKAGE_PATH=/opt/ros/indigo/stacks:$ROS_PACKAGE_PATH
