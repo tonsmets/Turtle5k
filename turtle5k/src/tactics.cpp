68f989f14a123252fd1beb5143cc1989297cf876
JtotheZ <joury.zimmermann@gmail.com>
2016-01-15 10:16:57 +0100

    Changed Tactics to send Twist messages in a loop instead of in a callback.

--- turtle5k/src/tactics.cpp
+++ turtle5k/src/tactics.cpp
@@ -12,6 +12,7 @@ ros::Publisher pShootPub;
 ros::Subscriber pPathSub;
 ros::Subscriber pWorldSub;
 ros::Subscriber pBallHandlingSub;
+geometry_msgs::Twist pTwistMessage;
 bool pBallGrabbed = false;
 
 geometry_msgs::Twist GetTwistMessage(turtle5k::WorldMessage aMessage)
@@ -43,6 +44,7 @@ void ballhandlingCallback(const turtle5k::BallHandlingMessage aMessage)
 
 void worldCallback(const turtle5k::WorldMessage& aMessage)
 {
+	memset(&pTwistMessage, 0, sizeof(pTwistMessage));
 	if(aMessage.objectType == OBJECT_GOAL && pBallGrabbed) {
 		if(aMessage.angleBetween < (8 / (180 / M_PI))) {
 			turtle5k::ShootMessage pMessage;
@@ -52,21 +54,25 @@ void worldCallback(const turtle5k::WorldMessage& aMessage)
 		}
 	}
 	else {
-		geometry_msgs::Twist pMessage = GetTwistMessage(aMessage);
-		pTwistPub.publish(pMessage);
+		pTwistMessage = GetTwistMessage(aMessage);
 	}
 }
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "t5k_tactics");
 	ros::NodeHandle pHandle;
+	ros::Rate pRate(100);
 	
 	pWorldSub = pHandle.subscribe("/world", 1000, worldCallback);
 	pTwistPub = pHandle.advertise<geometry_msgs::Twist>("/motorspeed_set", 1000);
 	pShootPub = pHandle.advertise<turtle5k::ShootMessage>("/shooting_shoot", 1000);
 	pBallHandlingSub = pHandle.subscribe("/ballhandling", 1000, ballhandlingCallback);
-	
-	ros::spin();
+	memset(&pTwistMessage, 0 , sizeof(pTwistMessage));
+	while(ros::ok()) {
+		pTwistPub.publish(pTwistMessage);
+		ros::spinOnce();
+		pRate.sleep();
+	}
 	
 	return 0;
 }
